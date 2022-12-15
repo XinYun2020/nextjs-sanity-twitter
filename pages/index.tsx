10a563b49ac651ea664d5b1e4b90c197f3dc0462
XinYun2020 <xinyunzhang2016@gmail.com>
2022-12-16 01:40:42 +1100

    getServerSideProps => fetchTweets make api call to backend from frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,16 @@ import Image from "next/image";
 import Feed from "../src/components/Feed";
 import Sidebar from "../src/components/Sidebar";
 import Widgets from "../src/components/Widgets";
+import { Tweet } from "../typings";
+import { fetchTweets } from "../utils/fetchTweets";
 
-const Home: NextPage = () => {
+interface Props {
+  tweets: Tweet[];
+}
+
+const Home = ({ tweets }: Props) => {
+  console.log(tweets);
+
   return (
     <div className=" mx-auto max-h-screen overflow-hidden lg:max-w-6xl">
       <Head>
@@ -33,11 +41,11 @@ const Home: NextPage = () => {
 
 export default Home;
 
-// export const getServerSideProps: GetServerSideProps = async (context) => {
-//   // create a type tweets
-//   const tweets = await fetchTweets();
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  // create a type tweets
+  const tweets = await fetchTweets();
 
-//   return {
-//     props: {},
-//   };
-// };
+  return {
+    props: { tweets },
+  };
+};
