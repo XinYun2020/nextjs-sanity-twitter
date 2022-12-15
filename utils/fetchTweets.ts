92cd6e8ea86af025ad2544049f0d358e62f82827
XinYun2020 <xinyunzhang2016@gmail.com>
2022-12-16 01:33:20 +1100

    fetchTweets api call to the backend

--- utils/fetchTweets.ts
+++ utils/fetchTweets.ts
@@ -1,3 +1,15 @@
+import { Tweet } from "../typings";
+
+// able to use at frontend
 export const fetchTweets = async () => {
   // make a rest api call to the backend
+  // only allow use absolute url, make connection to the endpoint
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getTweets`);
+
+  // get data from the res, pass the json response so we get back
+  const data = await res.json();
+  // cast tweet
+  const tweets: Tweet[] = data.tweets;
+
+  return tweets;
 };
